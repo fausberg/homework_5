9be8ce28564842f1a6901e719f81057b47459ea8
fausberg <ivanfadeev2003@mail.ru>
2022-09-14 13:16:02 +0300

    рщьу

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,29 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 //        Task_1();
-        Task_2();
+//        Task_2();
+//        Task_3();
+        Task_4();
     }
 
     public static void Task_1() {
-        int[][] arr = {{1, 2, 3, 4, 5,}, {1, 2, 3, 4, 5, 6}};
+        Random random = new Random();
+        int[][] arr = new int [3][3];
         for (int i = 0; i < arr.length; i++) {
             for (int g = 0; g < arr[i].length; g++) {
+                arr[i][g] = random.nextInt(10);
                 System.out.print(arr[i][g] + " ");
             }
             System.out.println();
         }
         System.out.println();
-        int[][] arr_1 = {{0,0,0,0,0,0},{0,0,0,0,0}};
+        int k = arr.length-1;
+        int[][] arr_1 = new int[3][3];
         for(int i = 0; i < arr.length; i++){
             for (int g = 0; g < arr[i].length;g++){
-                if(i == 0) {
-                    arr_1[1][g] = arr[i][g];
-                }else{
-                    arr_1[0][g] = arr[i][g];
-                }
+                arr_1[k][g] = arr[i][g];
             }
+            k--;
         }
         for (int i = 0; i < arr_1.length; i++) {
             for (int g = 0; g < arr_1[i].length; g++) {
@@ -36,41 +38,81 @@ public class Main {
     }
 
     public static void Task_2(){
-        int[][] arr = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5, 6}};
+        Random random = new Random();
+        int[][] arr = new int [4][4];
         for (int i = 0; i < arr.length; i++) {
             for (int g = 0; g < arr[i].length; g++) {
+                arr[i][g] = random.nextInt(10);
                 System.out.print(arr[i][g] + " ");
             }
             System.out.println();
         }
         System.out.println();
-        int pum = 0;
+        int[][] arr_1 = new int[4][4];
         for(int i = 0; i < arr.length; i++){
+            int k = arr[0].length-1;
             for (int g = 0; g < arr[i].length;g++){
-                if(g == 0) {
-                    pum = arr[0][0];
-                    arr[0][0] = arr[i][arr[i].length - 1];
-                    arr[i][arr[i].length - 1] =pum;
-                    break;
-                }
+                arr_1[i][k] = arr[i][g];
+                k--;
             }
         }
-        pum = 0;
-        for(int i = 1; i < arr.length; i++){
-            for (int g = 0; g < arr[i].length;g++){
-                if(g == 0){
-                    pum = arr[1][0];
-                    arr[1][0] = arr[1][arr[i].length - 1];
-                    arr[1][arr[i].length - 1] = pum;
-                    break;
-                }
+        for (int i = 0; i < arr_1.length; i++) {
+            for (int g = 0; g < arr_1[i].length; g++) {
+                System.out.print(arr_1[i][g] + " ");
             }
+            System.out.println();
         }
+    }
+
+    public static void Task_3(){
+        Random random = new Random();
+        int[][] arr = new int [4][4];
         for (int i = 0; i < arr.length; i++) {
             for (int g = 0; g < arr[i].length; g++) {
+                arr[i][g] = random.nextInt(10);
                 System.out.print(arr[i][g] + " ");
             }
             System.out.println();
         }
+        System.out.println();
+        int sum = 0;
+        int max = 0;
+        for(int i = 0; i < arr.length; i++){
+            for(int g = 0; g < arr[i].length; g++){
+                sum +=arr[i][g];
+            }
+            if(sum > max){
+                max = sum;
+            }
+            sum = 0;
+        }
+
+        System.out.println(max);
+    }
+
+    public static void Task_4(){
+        Random random = new Random();
+        int[][] arr = new int [4][4];
+        for (int i = 0; i < arr.length; i++) {
+            for (int g = 0; g < arr[i].length; g++) {
+                arr[i][g] = random.nextInt(10);
+                System.out.print(arr[i][g] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        int sum = 0;
+        int max = 0;
+        for(int i = 0; i < arr.length; i++){
+            for(int g = 0; g < arr[i].length; g++){
+                sum +=arr[g][i];
+            }
+            if(sum > max){
+                max = sum;
+            }
+            sum = 0;
+        }
+
+        System.out.println(max);
     }
 }
