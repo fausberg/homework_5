c90d07e7f515732cc5d40d9c82c069fd2e3dd457
fausberg <ivanfadeev2003@mail.ru>
2022-09-14 12:56:34 +0300

    рщьу

--- src/Main.java
+++ src/Main.java
@@ -36,7 +36,7 @@ public class Main {
     }
 
     public static void Task_2(){
-        int[][] arr = {{1, 2, 3, 4, 5, 0}, {1, 2, 3, 4, 5, 6}};
+        int[][] arr = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5, 6}};
         for (int i = 0; i < arr.length; i++) {
             for (int g = 0; g < arr[i].length; g++) {
                 System.out.print(arr[i][g] + " ");
